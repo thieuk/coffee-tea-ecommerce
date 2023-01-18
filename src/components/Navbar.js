2efb19544aad66e1580d3233479e3d126fcb94b1
Khai Thieu <khaithieu3@gmail.com>
2023-01-17 23:13:27 -0600

    changed the product button style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,12 @@ function dropdown() {
     const dropdownBttn = document.querySelector(".dropdown-bttn");
     const dropdownItems = document.querySelector(".dropdown-items");
 
-    dropdownBttn.addEventListener("mouseover", () => { dropdownItems.style.display = "block" });
+    dropdownBttn.addEventListener("mouseover", () => { dropdownItems.style.display = "block"; });
     dropdownBttn.addEventListener("click", () => { dropdownItems.style.display = "block" });
-    dropdownItems.addEventListener("mouseover", () => { dropdownItems.style.display = "block" });
+    dropdownItems.addEventListener("mouseover", () => { dropdownItems.style.display = "block"; });
 
-    dropdownBttn.addEventListener("mouseout", () => { dropdownItems.style.display = "none" });
-    dropdownItems.addEventListener("mouseout", () => { dropdownItems.style.display = "none" });
+    dropdownBttn.addEventListener("mouseout", () => { dropdownItems.style.display = "none"; });
+    dropdownItems.addEventListener("mouseout", () => { dropdownItems.style.display = "none"; });
 }
 
 export default function Navbar() {
@@ -19,12 +19,12 @@ export default function Navbar() {
             <Link to="/" className="lg:text-xl text-lg font-bold md:block hidden">COFFEE & TEA</Link>
             <div className="flex items-center ml-auto sm:gap-9 md:gap-5 gap-2 font-bold sm:text-base text-xs">
                 <Link to="/" className="hover:border-b-4 border-black sm:mr-4 mr-3">Home</Link>
-                <Link to="/" className="hover:border-b-4 border-black sm:mr-4 mr-3">About</Link>
+                <Link to="/about" className="hover:border-b-4 border-black sm:mr-4 mr-3">About</Link>
                 <div className="cursor-pointer">
-                    <div className="dropdown-bttn flex items-center" onMouseEnter={dropdown} onMouseOut={dropdown} onClick={dropdown}>
+                    <div className="dropdown-bttn flex items-center py-5" onMouseEnter={dropdown} onMouseOut={dropdown} onClick={dropdown}>
                         Products<span className="material-symbols-outlined">expand_more</span>
                     </div>
-                    <div className="dropdown-items absolute hidden font-normal z-[1] bg-white shadow-lg bg-neutral-100 ml-[-35px] p-3"
+                    <div className="dropdown-items absolute hidden font-normal z-[1] shadow-xl bg-neutral-100 ml-[-35px] p-3 border-t-4 border-black"
                         onMouseEnter={dropdown} onMouseOut={dropdown}>
                         <Link to="/product/coffee" className="block mt-3 py-2 text-center hover:bg-neutral-200 p-3">Coffee</Link>
                         <Link to="/product/tea" className="block mt-3 py-2 text-center hover:bg-neutral-200 p-3">Tea</Link>
