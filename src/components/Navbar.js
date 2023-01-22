16658350ea29b1fd0356de8eb8f76fe9f970202d
Khai Thieu <khaithieu3@gmail.com>
2023-01-22 12:38:53 -0600

    fixed nav warnings

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,8 +34,8 @@ export default function Navbar() {
                 <Link to="/contact" className="hover:border-b-4 border-black">Contact</Link>
                 <Link to="/checkout" title="Checkout" className="material-symbols-outlined">shopping_cart_checkout</Link>
                 <Link to="/account" title="Account">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-7 h-7 mr-7">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-7 h-7 mr-7">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                     </svg>
                 </Link>
             </div>
