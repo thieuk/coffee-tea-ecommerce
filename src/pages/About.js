41dd48ddce71d15582059b1d0aaa6a3b144bd9f0
Khai Thieu <khaithieu3@gmail.com>
2023-02-08 09:20:18 -0600

    changed the text on the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,20 +5,22 @@ export default function About() {
                 <h1 className="text-center text-6xl text-white font-bold">About us</h1>
             </div>
             <div className="sm:w-3/4 w-full max-w-[800px] mx-auto mt-[-75px] text-lg bg-white rounded border-2 shadow-xl p-7">
-                <p>Welcome to our online tea and coffee shop! We are a dedicated team of tea and coffee enthusiasts who 
-                believe that a great cup of tea or coffee can make all the difference in your day. We source only the highest 
-                quality teas and coffees from around the world, carefully selecting blends and single origin options to cater 
-                to a wide range of tastes. Whether you're a tea lover looking for a classic Earl Grey or a coffee connoisseur 
-                searching for the perfect pour-over blend, we have something for you.</p> 
-                <br />
+                <p>Welcome to our coffee and tea store! We're a team of coffee and tea lovers who are passionate about bringing the 
+                best blends and varieties to your doorstep. <br /> <br />
 
-                <p>In addition to our wide selection of teas and coffees, we also offer a variety of brewing equipment and 
-                accessories to help you make the perfect cup at home. From traditional teapots to state-of-the-art coffee makers, 
-                we have everything you need to elevate your at-home brewing experience. We pride ourselves on providing excellent 
-                customer service, and we're always happy to answer any questions you might have about our products or brewing methods.</p>
-                <br />
+                With a wide selection of coffee beans and loose-leaf teas, we have something for everyone, whether you prefer bold and robust 
+                coffee or delicate and floral tea. We carefully source our products from some of the finest coffee and tea estates and suppliers 
+                around the world, ensuring that you receive only the highest quality products. <br /> <br />
 
-                <p>Thank you for visiting our online store, and we hope you enjoy shopping with us!</p>
+                Our mission is to provide you with an enjoyable and seamless shopping experience, from browsing our products to receiving your 
+                order. Our website is designed to be user-friendly and easy to navigate, making it simple for you to find what you're looking for. 
+                Our team is dedicated to providing excellent customer service and ensuring that every order is delivered promptly and in perfect 
+                condition. <br /> <br />
+
+                We believe that great coffee and tea should be accessible to everyone, and that's why we offer competitive prices without 
+                compromising on quality. We also have a commitment to sustainability and use eco-friendly packaging whenever possible. <br /> <br />
+
+                Thank you for choosing us as your go-to source for coffee and tea. We hope you'll find your perfect cup with us!</p>
             </div>
         </div>
     );
