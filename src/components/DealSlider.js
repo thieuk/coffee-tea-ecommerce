38527f2e5c3e6f017ec17687d4183c18273def66
Khai Thieu <khaithieu3@gmail.com>
2023-02-07 22:52:10 -0600

    added homepage

---
+++ src/components/DealSlider.js
@@ -0,0 +1,67 @@
+let currLocation = 0;
+
+function slideLeft() {
+    if (currLocation < 0) {
+        currLocation += 100;
+        document.getElementById("saleItems").style.transform = `translate(${currLocation}vw)`;
+        document.getElementById("saleItemLoc").children.item(Math.abs(currLocation) / 100 + 1).style.backgroundColor = "white";
+        document.getElementById("saleItemLoc").children.item(Math.abs(currLocation) / 100).style.backgroundColor = "#581c87";
+    }
+}
+
+function slideRight() {
+    if (currLocation > -400) {
+        currLocation -= 100;
+        document.getElementById("saleItems").style.transform = `translate(${currLocation}vw)`;
+        document.getElementById("saleItemLoc").children.item(Math.abs(currLocation) / 100 - 1).style.backgroundColor = "white";
+        document.getElementById("saleItemLoc").children.item(Math.abs(currLocation) / 100).style.backgroundColor = "#581c87";
+    }
+}
+
+export default function DealSlider(props) {
+    const saleAmount = 0.25;
+
+    return (
+        <div id="slider" className="relative">
+            <hr className="absolute w-full top-5 z-[-1] border-t-4 border-purple-800" />
+            <div className="w-fit mx-auto px-6 bg-white">
+                <h1 className="w-fit mt-9 text-center text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-purple-600 to-pink-600 drop-shadow-lg">
+                    Deals of the Week
+                </h1>
+            </div>
+            <div className="border-b-4 border-purple-800 overflow-x-hidden">
+                <div id="saleItems" className="flex w-[500vw] transition-transform duration-1000">
+                    {props.items.map((item) => (
+                        <div className="w-full flex justify-center items-center my-9">
+                            <img src={require(`../img/${item.img}`)} alt="featured coffee" className="md:h-[175px] h-[135px]"/>
+                            <div>
+                                <h1 className="font-bold md:text-3xl sm:text-2xl text-xl">{item.brand}</h1>
+                                <h2 className="font-bold md:text-xl sm:text-lg text-base">{item.item}</h2>
+                                <p className="md:text-base text-sm">
+                                    <span className="text-neutral-500 mr-2 line-through">${item.price}</span>
+                                    ${(parseFloat(item.price) - parseFloat(item.price) * saleAmount).toFixed(2)}
+                                </p>
+                                <button className="bg-amber-400 mx-auto px-2 py-1 mt-2 rounded text-black font-bold hover:bg-yellow-500 md:text-base text-sm">
+                                    View Product
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div id="saleItemLoc" className="w-full flex justify-center gap-3 mb-4">
+                    <span className="rounded-full w-[10px] aspect-square border-2 border-purple-900 bg-purple-900"></span>
+                    <span className="rounded-full w-[10px] aspect-square border-2 border-purple-900"></span>
+                    <span className="rounded-full w-[10px] aspect-square border-2 border-purple-900"></span>
+                    <span className="rounded-full w-[10px] aspect-square border-2 border-purple-900"></span>
+                    <span className="rounded-full w-[10px] aspect-square border-2 border-purple-900"></span>
+                </div>
+                <button onClick={slideLeft} className="absolute top-[50%] left-0 h-[150px] translate-y-[calc(-50%+10px)] flex items-center p-2 bg-neutral-300 hover:bg-neutral-400">
+                    <span className="material-symbols-outlined">arrow_back_ios</span>
+                </button>
+                <button onClick={slideRight} className="absolute top-[50%] right-0 h-[150px] translate-y-[calc(-50%+10px)] flex items-center p-2 bg-neutral-300 hover:bg-neutral-400">
+                    <span className="material-symbols-outlined">arrow_forward_ios</span>
+                </button>                
+            </div>
+        </div>
+    );
+}
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,17 @@
 export default function Item(prop) {
     return (
-        <div className="flex flex-col items-center border-2 border-neutral-200 shadow-lg rounded h-fit 
-        sm:p-5 py-5 sm:m-5 m-2 md:w-[250px] sm:w-[200px] w-[150px]">
-            <div className="w-[80%] md:h-[200px] sm:h-[150px] h-[100px] pb-2">
+        <div className="flex flex-col items-center border-2 border-neutral-200 shadow-lg rounded h-fit sm:p-5 py-5 sm:m-5 m-2 md:w-[250px] sm:w-[200px] w-[150px]">
+            <div className="w-[80%] md:h-[160px] sm:h-[120px] h-[60px] pb-2">
                 <img src={require(`../img/${prop.img}`)} alt="item" className="h-full mx-auto object-contain"/>
             </div>
-            <h1 className="w-[80%] text-center sm:text-lg text-md font-bold text-ellipsis overflow-hidden whitespace-nowrap">
+            <h1 className="w-[80%] text-center sm:text-lg text-md font-extrabold text-ellipsis overflow-hidden whitespace-nowrap">
                 {`${prop.brand}`}
             </h1>
-            <h2 className="w-[80%] text-center sm:text-lg text-md text-ellipsis overflow-hidden whitespace-nowrap">{`${prop.item}`}</h2>
+            <h2 className="w-[80%] text-center sm:text-lg text-md text-ellipsis font-bold overflow-hidden whitespace-nowrap">{`${prop.item}`}</h2>
             <p className="sm:text-md text-sm ">{`$${prop.price}`}</p>
-        </div>
-                    
+            <button className="font-bold text-purple-900 border-2 border-purple-900 rounded-2xl px-2 py-0 m-2 md:px-2 md:py-1 md:text-base text-sm hover:bg-purple-900 hover:text-amber-400">
+                Add to Cart
+            </button>
+        </div>  
     );
 }
